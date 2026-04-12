a94ef82f703f185f8672f4c1cfd8d8270020cdf9
Albano Falcao Moura <71731018+albanofalcao@users.noreply.github.com>
2026-04-12 11:03:43 -0300

    Update migrate.js

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,25 +1,28 @@
-import { db } from './connection.js';
+import { dbAdmin } from './connection.js';
 import { up as up001 } from './migrations/001_create_groups.js';
 import { up as up002 } from './migrations/002_create_arenas.js';
 import { up as up003 } from './migrations/003_create_arena_responsaveis.js';
 import { up as up004 } from './migrations/004_create_arena_quadras.js';
+import { up as up005 } from './migrations/005_disable_rls.js';
 
 async function migrate() {
   console.log('Rodando migrations...');
   try {
-    await up001(db);
+    await up001(dbAdmin);
     console.log('001 - grupos_economicos OK');
-    await up002(db);
+    await up002(dbAdmin);
     console.log('002 - arenas OK');
-    await up003(db);
+    await up003(dbAdmin);
     console.log('003 - arena_responsaveis OK');
-    await up004(db);
+    await up004(dbAdmin);
     console.log('004 - arena_quadras OK');
+    await up005(dbAdmin);
+    console.log('005 - RLS desabilitado OK');
     console.log('Migrations concluidas!');
   } catch (e) {
     console.error('Erro na migration:', e.message);
   } finally {
-    await db.destroy();
+    await dbAdmin.destroy();
   }
 }
 
