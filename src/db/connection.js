860cf877d9f80148f5566038a4265627cbac3c2e
Albano Falcao Moura <71731018+albanofalcao@users.noreply.github.com>
2026-04-12 10:15:23 -0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,13 @@
 import knex from 'knex';
-import pg from 'pg';
-
-pg.defaults.ssl = { rejectUnauthorized: false };
 
 export const db = knex({
   client: 'pg',
   connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+export const dbAdmin = knex({
+  client: 'pg',
+  connection: process.env.DATABASE_ADMIN_URL,
+  ssl: { rejectUnauthorized: false }
 });
